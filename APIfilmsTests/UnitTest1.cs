88bb98aace1455bd1912d6a973a05cc18a32c194
oeoecbien <oeoecbienmoi@gmail.com>
2025-02-27 17:42:28 +0100

    P1 FINISH

---
+++ APIfilmsTests/UnitTest1.cs
@@ -0,0 +1,102 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.EntityFrameworkCore;
+using APIfilms.Models.EntityFramework;
+using APIfilms.Controllers;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIfilms.Tests
+{
+    [TestClass]
+    public class UtilisateursControllerTests
+    {
+        private FilmRatingsDBContext context;
+        private UtilisateursController controller;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var options = new DbContextOptionsBuilder<FilmRatingsDBContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            context = new FilmRatingsDBContext(options);
+            controller = new UtilisateursController(context);
+        }
+
+        [TestMethod]
+        public async Task GetAllUtilisateurs_ReturnsAllUsers()
+        {
+            var result = await controller.GetUtilisateurs();
+            Assert.AreEqual(context.Utilisateurs.Count(), result.Value.Count());
+        }
+
+        [TestMethod]
+        public async Task GetUtilisateurById_ReturnsCorrectUser()
+        {
+            var user = new Utilisateur { Nom = "Test", Prenom = "User", Mail = "test@example.com" };
+            context.Utilisateurs.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await controller.GetUtilisateurById(user.UtilisateurId);
+            Assert.IsNotNull(result.Value);
+            Assert.AreEqual(user.Mail, result.Value.Mail);
+        }
+
+        [TestMethod]
+        public async Task GetUtilisateurById_ReturnsNotFound()
+        {
+            var result = await controller.GetUtilisateurById(999);
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task PostUtilisateur_CreatesNewUser()
+        {
+            Random rnd = new Random();
+            int uniqueId = rnd.Next(1, 1000000000);
+
+            var newUser = new Utilisateur
+            {
+                Nom = "Dupont",
+                Prenom = "Jean",
+                Mail = $"dupont{uniqueId}@test.com",
+                Pwd = "Test1234!"
+            };
+
+            var result = await controller.PostUtilisateur(newUser);
+            var createdUser = await context.Utilisateurs.Where(u => u.Mail == newUser.Mail).FirstOrDefaultAsync();
+
+            Assert.IsNotNull(createdUser);
+            Assert.AreEqual(newUser.Mail, createdUser.Mail);
+        }
+
+        [TestMethod]
+        public async Task PostUtilisateur_DuplicateEmail_ReturnsConflict()
+        {
+            var user = new Utilisateur { Nom = "Test", Prenom = "User", Mail = "duplicate@example.com" };
+            context.Utilisateurs.Add(user);
+            await context.SaveChangesAsync();
+
+            var duplicateUser = new Utilisateur { Nom = "Test2", Prenom = "User2", Mail = "duplicate@example.com" };
+            var result = await controller.PostUtilisateur(duplicateUser);
+
+            Assert.IsInstanceOfType(result.Result, typeof(ConflictObjectResult));
+        }
+
+        [TestMethod]
+        public async Task DeleteUtilisateur_RemovesUser()
+        {
+            var user = new Utilisateur { Nom = "Test", Prenom = "User", Mail = "delete@example.com" };
+            context.Utilisateurs.Add(user);
+            await context.SaveChangesAsync();
+
+            var result = await controller.DeleteUtilisateur(user.UtilisateurId);
+            var deletedUser = await context.Utilisateurs.FindAsync(user.UtilisateurId);
+
+            Assert.IsNull(deletedUser);
+        }
+    }
+}
